b57de2eb686382359962d28c8cebb26a054dff4b
Zeavel <34531943+Zeavel@users.noreply.github.com>
2018-01-08 00:02:43 +0400

    Update mwomon.js

--- mwomon.js
+++ mwomon.js
@@ -99,7 +99,7 @@ if(commandIs('uptime', message))
     const embed = new Discord.MessageEmbed()
       
 
-      .setAuthor("MWO-Monitoring", client.user.avatarURL)
+      .setAuthor("MWO-Monitoring", client.user.displayAvatarURL)
 
       .addField("Server uptime","**" + uptime[1] + "**")
 
